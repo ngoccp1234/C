dacb68542557f4cf3c35de231d77fa961bf7e84a
ngoccp1234 <ngoclath1807071@fpt.edu.vn>
2018-09-25 14:53:18 +0700

    bai-tap-ho-va-ten

--- hoten.c
+++ hoten.c
@@ -4,14 +4,11 @@
 void noiHoVaTen(char ho[], char ten[]) {
 	int dodaiho = strlen(ho);
     int dodaiten = strlen(ten);
-    int tong = dodaiho + dodaiten;
-    printf("Do dai cua ho la: %d\n", strlen(ho));
-    printf("Do dai cua ten la: %d\n", strlen(ten));
-    
+    int tong = dodaiho + dodaiten; 
     printf("Do dai cua ca ho va ten la %d\n", tong);
     tong++;
     char hovaten[tong];
-    strcat(hovaten, ho);
+    strcpy(hovaten, ho);
     strcat(hovaten, " ");
     strcat(hovaten, ten);
     printf("ho va ten la:%s", hovaten);
@@ -19,29 +16,17 @@ void noiHoVaTen(char ho[], char ten[]) {
 
 int main() {
 
-    char ho[50], ten[50];
-    while(1==1){
-    	printf("Vui long nhap ho cua ban: \n");
-    	gets(ho);
-    	if (strlen(ho) > 50){
-    		printf (" ho  qua dai vui long nhap lai\n");
-		}
-		else
-		break;
-	}
-  
-  while(1==1){
-  	printf("Vui long nhap ten cua ban:\n ");
-  	gets(ten);
-    	if (strlen(ten) > 50){
-    		printf (" ten qua dai vui long nhap lai\n");
-    		
-		}
-		else
-		break;
+    char ho[10], ten[10];
     
-    }
-    // ki?m tra d? dài tên.
+    	printf("Vui long nhap ho cua ban: \n");
+    	fgets(ho, 10, stdin);
+    	puts(ho);
+    	if (!strchr(ho, '\n')){
+    while (fgetc(stdin) != '\n');
+		}
+  	printf("Vui long nhap ten cua ban:\n ");
+  	fgets(ten, 10, stdin);
+  	puts (ten);
    noiHoVaTen(ho, ten);
     return 0;
 }
